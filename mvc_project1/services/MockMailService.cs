c688b467f0cf0f0b6717ebc22d6ae05a8ae5e267
athacker <andrea.thacker@outlook.net>
2015-03-26 15:15:56 -0600

    Mock Service for local development work.

--- mvc_project1/services/MockMailService.cs
+++ mvc_project1/services/MockMailService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace mvc_project1.Services
 {
     public class MockMailService : IMailService
     {
+
+        public bool SendMail(string from, string to, string subject, string body)
+        {
+            Debug.WriteLine(string.Concat("Send Mail: " + subject));
+            return true;
+        }
     }
 }
